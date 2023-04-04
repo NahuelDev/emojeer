4f491f819dc23d3262676de321b85a1397c7b5f8
Nahuel Clotet <nahuee2011@gmail.com>
2023-04-04 19:44:32 -0300

    Remove unused variables

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -1,4 +1,3 @@
-import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -6,14 +5,11 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUsersForClient";
 
-
-// Create a new ratelimiter, that allows 3 requests per 1 minute
-
 export const profileRouter = createTRPCRouter({
 
     getUserByUsername: publicProcedure
         .input(z.object({ username: z.string() }))
-        .query(async ({ ctx, input }) => {
+        .query(async ({ input }) => {
             const [user] = await clerkClient.users.getUserList({
                 username: [input.username]
             });
